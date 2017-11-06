9b6b00ed79b25fa8a50b2685a5b59ae2b8844e46
Devin <ddyson1@ithaca.edu>
2017-11-05 21:11:42 -0500

    advanced solution palindrome

--- basicAlgorithmicScripting/check-for-palindromes.js
+++ basicAlgorithmicScripting/check-for-palindromes.js
@@ -1,11 +1,31 @@
+//this solution performs at minimum 7x better, at maximum infinitely better.
+//read the explanation for the reason why. I just failed this in an interview.
 function palindrome(str) {
-  str = str.toLowerCase().replace(/[\W_]/g, '');
-  for(var i = 0, len = str.length - 1; i < len/2; i++) {
-    if(str[i] !== str[len-i]) {
-      return false;
+  //assign a front and a back pointer
+  let front = 0;
+  let back = str.length - 1;
+
+  //back and front pointers won't always meet in the middle, so use (back > front)
+  while (back > front) {
+    //increments front pointer if current character doesn't meet criteria
+    while ( str[front].match(/[\W_]/) ) {
+      front++;
+      continue;
     }
+    //decrements back pointer if current character doesn't meet criteria
+    while ( str[back].match(/[\W_]/) ) {
+      back--;
+      continue;
+    }
+    //finally does the comparison on the current character
+    if ( str[front].toLowerCase() !== str[back].toLowerCase() ) return false
+    front++;
+    back--;
   }
+
+  //if the whole string has been compared without returning false, it's a palindrome!
   return true;
+
 }
 
 palindrome("eye");
